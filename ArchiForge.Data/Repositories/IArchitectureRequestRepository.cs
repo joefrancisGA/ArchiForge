7fa655659f6acfb4dc11ff52c3b8a35371aab6d2
Joe Francis <jafrancis@comcast.net>
2026-03-11 01:18:13 -0400

    Add ArchictectureRequestRepository

---
+++ ArchiForge.Data/Repositories/IArchitectureRequestRepository.cs
@@ -0,0 +1,9 @@
+﻿using ArchiForge.Contracts.Requests;
+
+namespace ArchiForge.Data.Repositories;
+
+public interface IArchitectureRequestRepository
+{
+    Task CreateAsync(ArchitectureRequest request, CancellationToken cancellationToken = default);
+    Task<ArchitectureRequest?> GetByIdAsync(string requestId, CancellationToken cancellationToken = default);
+}
